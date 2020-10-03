720a366324e59989f5cb9edbc234e228af692d72
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-03 18:55:30 +0900

    pages controller 作成

---
+++ spec/requests/pages_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Pages", type: :request do
+
+  describe "GET /index" do
+    it "returns http success" do
+      get "/pages/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /terms" do
+    it "returns http success" do
+      get "/pages/terms"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
