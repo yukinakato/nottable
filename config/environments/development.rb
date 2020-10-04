7305e11376a782a3227a61aedd646275b7d78e92
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-04 12:36:14 +0900

    docker web_console 用設定変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.web_console.permissions = '192.168.0.0/16'
+  config.web_console.permissions = '0.0.0.0/0'
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
