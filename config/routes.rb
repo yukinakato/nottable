0f975c910d7c6ab48a4a24d7ef8b90a572656cc1
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-04 15:22:12 +0900

    users controller show action 作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/terms', to: 'pages#terms'
   devise_for :users, controllers: { registrations: 'registrations' }
+
+  resources :users, only: :show
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
