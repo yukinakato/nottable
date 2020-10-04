03e171875f6cf080711e4ed540561388ce0df67e
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-04 23:01:56 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
   get '/terms', to: 'pages#terms'
   devise_for :users, controllers: { registrations: 'registrations' }
 
-  resources :users, only: :show
+  resources :users, only: :show do
+    member do
+      get 'bookmark'
+    end
+  end
+  resources :notes
+  resources :bookmarks, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  resource :notifications, only: [:show, :destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
