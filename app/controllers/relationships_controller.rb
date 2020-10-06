fa636a23254dba2a4e5fadb98a3dfda55307992c
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-06 15:32:12 +0900

    フォロー機能追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,26 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    # もしユーザーが退会していた場合はルートにリダイレクト
+    @user = User.find(params[:followed_id])
+    redirect_to root_path and return if @user.nil?
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+
+  def destroy
+    # もしユーザーが退会していた場合はルートにリダイレクト
+    relationship = Relationship.find(params[:id])
+    redirect_to root_path and return if relationship.nil?
+    @user = relationship.followed
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,14 @@ class User < ApplicationRecord
   validates :display_name, presence: true, length: { maximum: Constants::USER_DISPLAY_NAME_MAX_LENGTH }
   validates :introduce, length: { maximum: Constants::USER_INTRODUCE_MAX_LENGTH }
 
-  # has_many :active_relationships, class_name: "Relationship",
-  #                                 foreign_key: "follower_id",
-  #                                 dependent: :destroy
-  # has_many :passive_relationships, class_name: "Relationship",
-  #                                  foreign_key: "followed_id",
-  #                                  dependent: :destroy
-  # has_many :following, through: :active_relationships, source: :followed
-  # has_many :followers, through: :passive_relationships, source: :follower
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   has_many :notes, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   has_many :bookmarked_notes, through: :bookmarks, source: :note
@@ -24,4 +24,21 @@ class User < ApplicationRecord
   def bookmarked?(note)
     bookmarked_notes.include?(note)
   end
+
+  def follow(user)
+    following << user
+  end
+
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    following.include?(user)
+  end
+
+  # 未読通知数を取得する
+  # def unread_notifications
+  #   notifications.where(unread: true).count
+  # end
 end
