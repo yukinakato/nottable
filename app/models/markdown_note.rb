7e7a0037793f8005d409e8205dd818ac5dfacc63
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-07 18:29:26 +0900

    Noteモデルに検索のscope作成

--- app/models/markdown_note.rb
+++ app/models/markdown_note.rb
@@ -2,4 +2,5 @@ class MarkdownNote < ApplicationRecord
   has_one :note, as: :note_entity
 
   validates :content, length: { maximum: Constants::NOTE_CONTENT_MAX_LENGTH }
+  # scope :search, -> (keyword) { where("content LIKE ?", "%#{keyword}%") }
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -5,4 +5,10 @@ class Note < ApplicationRecord
   validates :title, presence: true, length: { maximum: Constants::NOTE_TITLE_MAX_LENGTH }
 
   scope :no_private, -> { where(private: false) }
+  scope :search, -> (keyword) {
+    joins("LEFT OUTER JOIN markdown_notes ON markdown_notes.id = notes.note_entity_id").
+    joins("LEFT OUTER JOIN rich_notes ON rich_notes.id = notes.note_entity_id").
+    joins("LEFT OUTER JOIN action_text_rich_texts ON action_text_rich_texts.record_id = rich_notes.id").
+    where("title LIKE ? OR (note_entity_type = 'MarkdownNote' AND markdown_notes.content LIKE ?) OR (note_entity_type = 'RichNote' AND action_text_rich_texts.body LIKE ?)", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%")
+  }
 end
