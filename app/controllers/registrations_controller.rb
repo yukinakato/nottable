1750ef69d05208a352d2c4b1939badf6c8511a8e
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-07 21:19:08 +0900

    プロフィール、パスワード変更実装

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,23 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters  
+
+  def edit_password
+  end
+
+  def update_password
+    if current_user.valid_password?(params[:user][:current_password])
+      if current_user.update(new_passwords)
+        flash[:success] = "パスワードを変更しました"
+        bypass_sign_in(current_user)
+        redirect_to edit_user_registration_path
+      else
+        render 'devise/registrations/edit_password'
+      end
+    else
+      flash.now[:danger] = "現在のパスワードが相違しています"
+      render 'devise/registrations/edit_password'
+    end
+  end
   
   protected
 
@@ -15,5 +33,10 @@ class RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:display_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:display_name, :introduce, :password, :password_confirmation])
+  end
+
+  def new_passwords
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/terms', to: 'pages#terms'
   devise_for :users, controllers: { registrations: 'registrations' }
+  devise_scope :user do
+    get '/users/edit/password', to: "registrations#edit_password"
+    patch '/users/edit/password', to: "registrations#update_password"
+  end
 
   resources :users, only: :show do
     member do
