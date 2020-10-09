7edff52409b77eec7db07c9a1e45d66c148583d3
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-09 18:06:18 +0900

    flash をフェードアウトで消す

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < Devise::SessionsController
   def guest_sign_in
     guest_user = User.find_by(email: Constants::GUEST_EMAIL)
     sign_in guest_user
+    set_flash_message!(:notice, :signed_in)
     redirect_to root_path
   end
 end
---
+++ app/javascript/packs/flash_fadeout.js
@@ -0,0 +1,1 @@
+$(".flash-fadeoutable").delay(2800).fadeOut(1000)
