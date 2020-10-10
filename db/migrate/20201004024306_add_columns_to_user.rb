f401b6b8c5fcc898bd4121a3fe3f5d27abc8046a
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-10 15:32:33 +0900

    自己紹介をtextにする

--- db/migrate/20201004024306_add_columns_to_user.rb
+++ db/migrate/20201004024306_add_columns_to_user.rb
@@ -1,7 +1,7 @@
 class AddColumnsToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :display_name, :string, null: false
-    add_column :users, :introduce, :string
+    add_column :users, :introduce, :text
     add_column :users, :provider, :string
     add_column :users, :uid, :string
     add_index :users, :display_name
