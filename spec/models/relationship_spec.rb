ed5163fbd863a1042ac1aa328b92223c7d9a530b
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-13 23:59:03 +0900

    relationship model spec 作成

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "存在のバリデーションテスト" do
+    subject { relationship }
+
+    let(:follower) { create(:user) }
+    let(:followed) { create(:user) }
+
+    context "フォロワー、フォロイーが存在する時" do
+      let(:relationship) { build(:relationship, follower: follower, followed: followed) }
+
+      it { is_expected.to be_valid }
+    end
+
+    context "フォロワーしか存在しない時" do
+      let(:relationship) { build(:relationship, follower: follower, followed: nil) }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context "フォロイーしか存在しない時" do
+      let(:relationship) { build(:relationship, followed: followed, follower: nil) }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context "フォロワーもフォロイーも存在しない時" do
+      let(:relationship) { build(:relationship, followed: nil, follower: nil) }
+
+      it { is_expected.not_to be_valid }
+    end
+  end
+
+  describe "フォロー関係の重複ができないことのテスト" do
+    let(:follower) { create(:user) }
+    let(:followed) { create(:user) }
+
+    it "データベースのユニーク制約エラーが発生する" do
+      create(:relationship, follower: follower, followed: followed)
+      expect { create(:relationship, follower: follower, followed: followed) }.to raise_error ActiveRecord::RecordNotUnique
+    end
+  end
 end
