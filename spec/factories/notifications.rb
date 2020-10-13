b7f48b1ff69bbaa37eef11b44495a8c772b54a94
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-13 23:59:20 +0900

    notification model spec 作成

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,6 +1,18 @@
 FactoryBot.define do
   factory :notification do
-    user { nil }
-    unread { false }
+    user
+    for_note
+
+    trait :for_note do
+      association :notify_entity, :markdown, factory: :note
+    end
+  
+    trait :for_follow do
+      association :notify_entity, factory: :relationship
+    end
+  
+    trait :for_bookmark do
+      association :notify_entity, factory: :bookmark
+    end
   end
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "ユーザーの有無のバリデーションテスト" do
+    subject { notification }
+
+    let(:user) { create(:user) }
+
+    context "ユーザーが存在しない時" do
+      let(:notification) { build(:notification, user: nil) }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context "ユーザーが存在する時" do
+      let(:notification) { build(:notification, user: user) }
+
+      it { is_expected.to be_valid }
+    end
+  end
+
+  describe "entity の有無のバリデーションテスト" do
+    subject { notification }
+
+    let(:user) { create(:user) }
+
+    context "ノート作成に対する通知の時" do
+      let(:notification) { build(:notification, :for_note, user: user) }
+
+      it { is_expected.to be_valid }
+    end
+
+    context "フォローに対する通知の時" do
+      let(:notification) { build(:notification, :for_follow, user: user) }
+
+      it { is_expected.to be_valid }
+    end
+
+    context "ブックマークに対する通知の時" do
+      let(:notification) { build(:notification, :for_bookmark, user: user) }
+
+      it { is_expected.to be_valid }
+    end
+
+    context "entity がない時" do
+      let(:notification) { build(:notification, notify_entity: nil, user: user) }
+
+      it { is_expected.not_to be_valid }
+    end
+  end
+
+  describe "latest スコープのテスト" do
+    it "最新順に並び替えられる" do
+      n_1 = create(:notification)
+      n_2 = create(:notification)
+      n_3 = create(:notification)
+      expect(Notification.latest).to eq [n_3, n_2, n_1]
+    end
+  end
 end
