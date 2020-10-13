3b331c719697e63be79910793268873949c4078f
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-13 23:58:43 +0900

    bookmark model spec 修正

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -5,18 +5,22 @@ RSpec.describe Bookmark, type: :model do
     subject { bookmark }
 
     context "ユーザー、ノートが存在する時" do
-      let(:bookmark) { build(:bookmark) }
+      let(:user) { create(:user) }
+      let(:note) { create(:note) }
+      let(:bookmark) { build(:bookmark, user: user, note: note) }
 
       it { is_expected.to be_valid }
     end
 
     context "ユーザーしか存在しない時" do
+      let(:user) { create(:user) }
       let(:bookmark) { build(:bookmark, note: nil) }
 
       it { is_expected.not_to be_valid }
     end
 
     context "ノートしか存在しない時" do
+      let(:note) { create(:note) }
       let(:bookmark) { build(:bookmark, user: nil) }
 
       it { is_expected.not_to be_valid }
@@ -26,6 +30,16 @@ RSpec.describe Bookmark, type: :model do
       let(:bookmark) { build(:bookmark, user: nil, note: nil) }
 
       it { is_expected.not_to be_valid }
+    end
+  end
+
+  describe "ブックマークの重複ができないことのテスト" do
+    let(:user) { create(:user) }
+    let(:note) { create(:note) }
+
+    it "データベースのユニーク制約エラーが発生する" do
+      create(:bookmark, user: user, note: note)
+      expect { create(:bookmark, user: user, note: note) }.to raise_error ActiveRecord::RecordNotUnique
     end
   end
 end
