521a124a454c7889066315a6ed4298f001be3333
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-13 23:58:28 +0900

    notification モデルにバリデーション追加

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,5 +2,8 @@ class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :notify_entity, polymorphic: true
 
+  validates :user_id, presence: true
+  validates :notify_entity, presence: true
+
   scope :latest, -> { order(created_at: :desc) }
 end
