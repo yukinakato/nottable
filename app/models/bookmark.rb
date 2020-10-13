e16156362bfd35ed529dccf0536d6811e5f59a74
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-13 23:58:13 +0900

    bookmark モデルにバリデーション追加

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,8 @@ class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :note
 
+  validates :user_id, presence: true
+  validates :note_id, presence: true
+
   has_one :notification, as: :notify_entity, dependent: :destroy
 end
