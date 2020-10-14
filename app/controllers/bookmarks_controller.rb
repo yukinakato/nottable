ad8f51c0106c14670a12c8c63a0f79598bc122e9
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-14 23:09:09 +0900

    bookmarks コントローラ修正、request spec 作成

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,31 +4,27 @@ class BookmarksController < ApplicationController
   def create
     bookmark = current_user.bookmarks.build(bookmark_params)
     @note = Note.find_by(id: bookmark.note_id)
-    if bookmark.save
-      # 自分で自分のコンテンツにいいねした時は通知を作成しない
+    if @note.nil? # 投稿が削除されている
+      redirect_to root_path
+      return
+    end
+    unless current_user.bookmarks.find_by(bookmark_params) # すでにブックマーク済みではない
+      bookmark.save
+      # 自分で自分のノートをブックマークした時は通知を作成しない
       if current_user != @note.user
-        Notification.create(user_id: @note.user_id, notify_entity: bookmark)
+        Notification.find_or_create_by(user_id: @note.user_id, notify_entity: bookmark)
       end
-      respond_to do |format|
-        format.html { redirect_to note_path(@note) }
-        format.js
-      end
-    else
-      if @note.nil?
-        # いいねに失敗（投稿が削除されていて見つからなかった）
-        redirect_to root_path
-      else
-        # いいねに失敗（すでにいいねしていた）
-        redirect_to note_path(@note)
-      end
+    end
+    respond_to do |format|
+      format.html { redirect_to note_path(@note) }
+      format.js
     end
   end
 
   def destroy
-    # 別タブでいいね解除をした場合、bookmark が nil になってしまいルートに飛んでしまう
     bookmark = Bookmark.find_by(id: params[:id])
     if bookmark.nil? || bookmark.user != current_user
-      redirect_to root_path
+      redirect_to(request.headers["HTTP_REFERER"] || root_path)
       return
     end
     @note = bookmark.note
--- spec/requests/bookmarks_request_spec.rb
+++ spec/requests/bookmarks_request_spec.rb
@@ -1,4 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe "Bookmarks", type: :request do
+  describe "単独の削除リクエストのテスト" do
+    context "自分のブックマークに対するリクエスト" do
+      let(:user) { create(:user) }
+      let!(:bookmark) { create(:bookmark, user: user) }
+
+      it "削除できる" do
+        sign_in user
+        expect { delete bookmark_path(bookmark) }.to change(Bookmark, :count).from(1).to(0)
+      end
+    end
+
+    context "他人のブックマークに対するリクエスト" do
+      let(:user) { create(:user) }
+      let(:other_user) { create(:user) }
+      let!(:bookmark) { create(:bookmark, user: other_user) }
+
+      it "削除できない" do
+        sign_in user
+        expect { delete bookmark_path(bookmark) }.not_to change(Bookmark, :count)
+      end
+    end
+
+    context "非ログイン状態" do
+      let!(:bookmark) { create(:bookmark) }
+
+      it "削除できない" do
+        expect { delete bookmark_path(bookmark) }.not_to change(Bookmark, :count)
+      end
+    end
+  end
+
+  describe "連続したリクエストのテスト" do
+    context "すでにブックマークずみの場合" do
+      let(:user) { create(:user) }
+      let(:note) { create(:note) }
+      let!(:bookmark) { create(:bookmark, user: user, note: note) }
+
+      it "ノートパスにリダイレクト" do
+        sign_in user
+        params = { bookmark: { user_id: user.id, note_id: note.id } }
+        post bookmarks_path, params: params
+        expect(response).to redirect_to note_path(note)
+      end
+    end
+
+    context "すでにブックマーク解除ずみの場合" do
+      let(:user) { create(:user) }
+      let(:note) { create(:note) }
+      let!(:bookmark) { create(:bookmark, user: user, note: note) }
+
+      it "ノートパスにリダイレクト" do
+        sign_in user
+        delete bookmark_path(bookmark)
+        delete bookmark_path(bookmark), headers: { "HTTP_REFERER": note_path(note) }
+        expect(response).to redirect_to note_path(note)
+      end
+    end
+  end
 end
