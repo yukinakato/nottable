5ec7d21b88ca87c823c6115292f870b1e87e13f4
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-15 16:05:07 +0900

    rubocop fix

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -6,11 +6,11 @@ FactoryBot.define do
     trait :for_note do
       association :notify_entity, :markdown, factory: :note
     end
-  
+
     trait :for_follow do
       association :notify_entity, factory: :relationship
     end
-  
+
     trait :for_bookmark do
       association :notify_entity, factory: :bookmark
     end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     sequence(:email) { |i| "user-#{format("%02d", i)}@example.com" }
     sequence(:display_name) { |i| "user-#{format("%02d", i)}" }
     password { "password" }
-  
+
     trait :with_avatar do
       avatar { Rack::Test::UploadedFile.new(Rails.root.join("spec", "factories", "cat.png")) }
     end
