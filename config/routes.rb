194b40460c1c5a06018144a2c98581bf5d39a8ce
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-16 15:22:03 +0900

    プライバシーポリシーページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "/terms", to: "pages#terms"
+  get "/privacy", to: "pages#privacy"
   devise_for :users,
              controllers: {
                registrations: "registrations",
