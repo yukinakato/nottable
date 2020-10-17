2305314dcc19c6a7e3778681b07fe2a6e7e1ffab
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-17 13:24:34 +0900

    不要ファイル、コメント削除

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,6 @@ module UsersHelper
   # ユーザーのプロフィール画像を表示する image tag を生成
   def get_user_avatar(user, size: 100)
     if user.avatar.attached?
-      # image_tag user.avatar.variant(resize: "#{size}x#{size}")
       image_tag user.avatar.variant(resize: "#{size}x#{size}^", gravity: "center", crop: "#{size}x#{size}+0+0")
     else
       image_tag 'defaultuser.png', width: "#{size}px"
